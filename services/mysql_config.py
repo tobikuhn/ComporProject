af558dd4182560e28bc778b1cc0d18fd06f34424
Thomas <t.kuhn@compor-ag.de>
2020-07-31 12:09:32 +0200

    Removed unnecessary comments

--- services/mysql_config.py
+++ services/mysql_config.py
@@ -1,4 +1,5 @@
 class MySQLConfig:
+    # jdbc:mysql://root:toor@192.168.129.142:3306/main
     MYSQL_DATABASE_HOST = "192.168.129.142"
     MYSQL_DATABASE_PORT = 3036
     MYSQL_DATABASE_USER = "root"
