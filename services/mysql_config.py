03a1d997cffba2fea70870bc782b51712a30826a
tommikuhn <68896373+tommikuhn@users.noreply.github.com>
2020-08-04 14:38:58 +0200

    Removed lcoal mysql config

--- services/mysql_config.py
+++
@@ -1,7 +0,0 @@
-class MySQLConfig:
-    # jdbc:mysql://comporplus:ShowHack!@localhost:3306/comporplus
-    MYSQL_DATABASE_HOST = "10.0.0.10"
-    MYSQL_DATABASE_PORT = 3036
-    MYSQL_DATABASE_USER = "comporplus"
-    MYSQL_DATABASE_PASSWORD = "ShowHack!"
-    MYSQL_DATABASE_DB = "comporplus"
