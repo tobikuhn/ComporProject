9346e82bc26f5b04bd648821a8b110896d38c413
root <root@compor-ag.de>
2022-10-22 17:46:32 +0200

    New internal services

---
+++ services/User.py
@@ -0,0 +1,14 @@
+from flask_login.mixins import UserMixin
+
+
+class User(UserMixin):
+
+    def __init__(self, id, email, password, name, **kwargs):
+        self.id = id
+        self.email = email
+        self.password = password
+        self.name = name
+
+    def generate_username(self):
+        split = self.email.split("@")
+        return split[0]
