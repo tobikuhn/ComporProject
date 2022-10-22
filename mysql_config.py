33ea2b601694bf59013223a937d78a49a4dc31a5
tobikuhn <68896373+tobikuhn@users.noreply.github.com>
2022-10-22 17:55:52 +0200

    Hide Credentials

--- mysql_config.py
+++ mysql_config.py
@@ -2,6 +2,6 @@ class MySQLConfig:
     # jdbc:mysql://comporproject:ShowHack!@localhost:3306/comporproject
     MYSQL_DATABASE_HOST = "localhost"
     MYSQL_DATABASE_PORT = 3036
-    MYSQL_DATABASE_USER = "comporproject"
-    MYSQL_DATABASE_PASSWORD = "ShowHack!"
-    MYSQL_DATABASE_DB = "comporproject"
+    MYSQL_DATABASE_USER = "********"
+    MYSQL_DATABASE_PASSWORD = "********"
+    MYSQL_DATABASE_DB = "********"
